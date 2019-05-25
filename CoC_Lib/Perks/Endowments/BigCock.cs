e592ae4f0f777e141833627555e9e5d138095a99
Caniz Declaw <caniz.declaw@gmail.com>
2019-05-25 11:30:48 -0400

    Minor bugfix.

--- CoC_Lib/Perks/Endowments/BigCock.cs
+++ CoC_Lib/Perks/Endowments/BigCock.cs
@@ -15,15 +15,21 @@ namespace CoC_Lib.Perks.Endowments
             "A bigger cock will make it easier to get off any sexual partners, " +
             "but only if they can take your size.";
 
-        public override void OnAddPerk(Creature creature)
+        public override void OnFirstTimeAdd(Creature creature)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnAddPerk(Creature creature, bool firstTime = true)
         {
 
         }
+
         public override void OnRemovePerk(Creature creature)
         {
 
         }
 
-        public override bool Qualified(Player player) => player.HasCock;
+        public override bool Qualified(Player player) => player.Body.HasCock;
     }
 }
